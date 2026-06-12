cb89b4e9e528a8237c5d7b7f11b8b77443dfdcbf
Roman Egorov <egorovr2555@gmail.com>
2026-06-12 12:43:26 +0300

    В models.py добавлены базовые модели каталога (Shop, Category, Product)

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,3 +1,135 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    """
+    Модель поставщика (магазина).
+    Содержит информацию о магазине и флаг, разрешающий или запрещающий
+    прием заказов от данного поставщика.
+    """
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+        unique=True,
+        help_text='Уникальное название магазина \ поставщика'
+    )
+    url = models.URLField(
+        verbose_name='Ссылка',
+        null=True,
+        blank=True,
+        help_text='URL поставщика'
+    )
+    state = models.BooleanField(
+        verbose_name='Статус приёма заказов',
+        default=True,
+        db_index=True,  # Индекс для быстрого поиска активных магазинов в БД
+        help_text='Включено - магазин принимает заказы, Выключено - импорт идет, но заказы не принимаются'
+    )
+
+    class Meta:
+        verbose_name = 'Магазин'
+        verbose_name_plural = 'Магазины'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    """
+    Категория товаров.
+    Привязана к магазину. ID категорий у разных
+    поставщиков могут совпадать, но означать разное.
+    """
+    id = models.PositiveIntegerField(
+        primary_key=True,
+        verbose_name='ID категории',
+        help_text='ID категории из прайс-листа поставщика'
+    )
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+        help_text='Название категории (например: Смартфоны)'
+    )
+    shop = models.ForeignKey(
+        Shop,
+        verbose_name='Магазин',
+        related_name='categories',
+        on_delete=models.CASCADE,
+        help_text='Магазин, к которому относится данная категория'
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['id', 'shop'],
+                name='unique_category_per_shop'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.name} (ID: {self.id})'
+
+
+class Product(models.Model):
+    """
+    Модель товара.
+    Содержит цены и наличие.
+    """
+    id = models.PositiveIntegerField(
+        primary_key=True,
+        verbose_name='ID товара',
+        help_text='Уникальный идентификатор товара из системы поставщика'
+    )
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Название',
+        help_text='Полное название товара'
+    )
+    model = models.CharField(
+        max_length=255,
+        verbose_name='Модель',
+        blank=True,
+        help_text='Техническое название модели (например: apple/iphone/xs-max)'
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name='Категория',
+        related_name='products',
+        on_delete=models.CASCADE,
+        help_text='Категория, к которой принадлежит товар'
+    )
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Цена',
+        validators=[MinValueValidator(0)],
+        help_text='Текущая цена закупки'
+    )
+    price_rrc = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Рекомендуемая розничная цена',
+        validators=[MinValueValidator(0)],
+        help_text='Рекомендуемая розничная цена (РРЦ)'
+    )
+    quantity = models.PositiveIntegerField(
+        verbose_name='Количество на складе',
+        help_text='Текущий остаток на складе поставщика'
+    )
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['id', 'category'],
+                name='unique_product_per_category'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.name} ({self.model})'
