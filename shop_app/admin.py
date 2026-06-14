e8a17bb9aa5a85afac3951d956e4753fb302df15
Roman Egorov <egorovr2555@gmail.com>
2026-06-14 14:34:55 +0300

    Добавлено отображение моделей в админке

--- shop_app/admin.py
+++ shop_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Shop, Category, Product, Parameter, ProductParameter
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'state')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'shop')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'model', 'price', 'quantity', 'category')
+
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(ProductParameter)
+class ProductParameterAdmin(admin.ModelAdmin):
+    list_display = ('product', 'parameter', 'value')
